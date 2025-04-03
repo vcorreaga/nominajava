7fbaba417140f3a4b3e351669310d9350d26b30e
vcorrega <viadis085@hotmail.com>
2025-04-02 21:06:07 -0500

    Se hace captura de valores y se calcula el salrio neto

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,7 @@ public class Main {
         double valorHora = 0;
         double cantidadHora = 0;
         double bonificacionEmpleado = 0;
+        double valorSalarioMinimo = 0;
         int auxilioTransporte = 200000;
 
         System.out.println("Ingrese su usuario:");
@@ -27,9 +28,36 @@ public class Main {
 
         if (usuarioEmpleado.equals("admin") && contrasenaUsuario.equals("admin123")) {
             System.out.println("Ha ingresado de forma correcta.");
-            } else {
+            System.out.println("Ingrese el nombre del empleado");
+            nombreEmpleado = sc.nextLine();
+            System.out.println("Ingrese su documento de identidad");
+            documentoIdentidad = sc.nextLine();
+            System.out.println("Ingrese su dia de descanso (Lunes a Viernes): ");
+            diaDescanso = sc.nextLine();
+            System.out.println("Ingrese el valor de la hora");
+            valorHora = sc.nextLine();
+            System.out.println("Ingrese la cantidad de horas trabajadas");
+            cantidadHora = sc.nextLine();
+            salarioBruto = valorHora * cantidadHora;
+            System.out.println("Ingrese el valor del salario minimo para este año: ");
+            valorSalarioMinimo = sc.nextDouble();
+            if (salarioBruto > (valorSalarioMinimo*2)){
+                bonificacionEmpleado = 0;
+                auxilioTransporte = 0;
+            }
+            else {
+                bonificacionEmpleado = salarioBruto*0.1;
+                auxilioTransporte = 200000;
+            }
+            salarioNeto = salarioBruto + bonificacionEmpleado + auxilioTransporte;
+            System.out.println("El salario neto es: " + salarioNeto);
+            }
+            else {
             System.out.println("Error de credenciales");
         }
         sc.close();
+        /*Otra Forma de hacerlo
+        *
+        * */
     }
 }
